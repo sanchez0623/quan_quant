851c5b08b82decba47a170212e59920326472bdb
Sanchez <zsq23837880@outlook.com>
2026-09-02 14:49:04 +0800

    feat(backtest): 回测区间快捷预设增加「最近两年」

--- frontend/src/components/BacktestRangePicker.tsx
+++ frontend/src/components/BacktestRangePicker.tsx
@@ -3,11 +3,12 @@ import dayjs, { type Dayjs } from 'dayjs'
 
 const { RangePicker } = DatePicker
 
-/** 回测区间快捷预设：最近一季/半年/一年/今年/去年 */
+/** 回测区间快捷预设：最近一季/半年/一年/两年/今年/去年 */
 const RANGE_PRESETS: Array<{ label: string; value: [Dayjs, Dayjs] }> = [
   { label: '最近一季', value: [dayjs().subtract(3, 'month'), dayjs()] },
   { label: '最近半年', value: [dayjs().subtract(6, 'month'), dayjs()] },
   { label: '最近一年', value: [dayjs().subtract(1, 'year'), dayjs()] },
+  { label: '最近两年', value: [dayjs().subtract(2, 'year'), dayjs()] },
   { label: '今年', value: [dayjs().startOf('year'), dayjs()] },
   {
     label: '去年',
